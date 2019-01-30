5d79eb565e3138d64a3633e89aa0fceab4a94a0e
Amina Jusic <amina_jusic@yahoo.com>
2019-01-30 09:40:02 +0000

    Changed studens to @students

--- interactive_dir_refactoring.rb
+++ interactive_dir_refactoring.rb
@@ -1,5 +1,5 @@
+@students = []
 def interactive_menu
-  students = []
   loop do
 
   print_menu
@@ -7,7 +7,7 @@ def interactive_menu
     # 3. do what the user has asked
     case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
       show_students
     when "9"
@@ -26,28 +26,26 @@ end
 
 def show_students
   print_header
-  print(students)
-  print_footer(students)
+  print
+  print_footer
 end
 
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  students =[]
+
   # get the first names
    name = gets.chomp
    # while the name is not empty, repaet this code
    while !name.empty? do
      # add the studen hash to the array
-     students << {name: name, cohort: :november}
-     puts "Now we have #{students.count} students"
+     @students << {name: name, cohort: :november}
+     puts "Now we have #{@students.count} students"
      #get another name from user
      name = gets.chomp
    end
-   #return the array of input_students
-   students
+
  end
 
 
@@ -57,16 +55,16 @@ puts "The students of Villains Academy"
 puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
+def print
+  @students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 
 
-def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+def print_footer
+puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
